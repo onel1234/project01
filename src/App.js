0eda87fbf7d0cb72514d3df5874ed4e93510e2c9
wathila <wathilaonelranaweera9@gmial.com>
2024-03-26 10:28:53 +0530

    mobile version new upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import './App.css';
-import imageSrc from './Screenshot 2024-03-19 101205.png'; 
-import LoginForm from './LoginForm';
+import './App.css'; 
+import imageSrc from './Screenshot 2024-03-19 101205.png';
 
 function App() {
   return (
     <div className="container">
-      <div className="content">
-        <div className="login-form-container">
-          <LoginForm />
-        </div>
-      </div>
       <div className="image-container">
+       
         <img src={imageSrc} alt="Image" className="image" />
+      </div>
+      <div className="login-container">
+        <div className="login-form">
+          <h2>WELCOME BACK!</h2>
+          <p> please enter your details to proceed</p>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" placeholder="Enter your username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Enter your password" />
+          </div>
+          <button className="login-button">Login</button>
+          <p className="create-account">Don't have an account? <a href="#">Create one</a></p>
+        </div>
       </div>
     </div>
   );
